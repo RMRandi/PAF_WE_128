d9f8b41b6e3130e5dc9b50a79dae55e700111a6d
Randimal Ranasinghe <90644824+RMRandi@users.noreply.github.com>
2022-04-25 17:04:25 +0530

    Interrupt crud  insert, update and delete commit

--- src/main/java/com/InterruptService.java
+++ src/main/java/com/InterruptService.java
@@ -25,6 +25,60 @@ public class InterruptService{
 		return itemObj.readInterrupt();
 	}
 	
+@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertInterrupt(@FormParam("interruptDate") String interruptDate, 
+	 @FormParam("interruptSubject") String interruptSubject, 
+	 @FormParam("interruptDesc") String interruptDesc)
+	
+	{ 
+	 String output = itemObj.insertInterrupt(interruptDate, interruptSubject, interruptDesc); 
+	return output; 
+	}
+
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData){
+		
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	 
+	//Read the values from the JSON object
+	 String ID = itemObject.get("interruptID").getAsString();
+	 String date = itemObject.get("interruptDate").getAsString();
+	 String sub = itemObject.get("interruptSubject").getAsString();
+	 String desc = itemObject.get("interruptDesc").getAsString();
+
+	 
+	 String output = itemObj.updateItem(ID,date, sub, desc);
+	 return output;
+	 
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteInterrupt(String interruptData)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(interruptData, "", Parser.xmlParser());
+
+	//Read the value from the element <itemID>
+	 String itemID = doc.select("itemID").text();
+	 String output = itemObj.deleteInterrupt(itemID);
+	return output;
+	}
+
+	
+	
+	
+	
 	
 	
 	
